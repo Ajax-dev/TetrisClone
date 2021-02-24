aab74826a20d275e89607610123fa9a7f3c2f640
Ajax-dev <ajwilt97@gmail.com>
2021-02-24 15:24:25 +0000

    Made all tetromino game objects and grid
    Tetrominoes fall and rotate, they stop when touching the walls, however on certain rotations they aren't allowed to reach the wall, TODO fix rotation problem

---
+++ Assets/Scripts/Behaviours/TetroMove.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TetroMove : MonoBehaviour
+{
+    public static int gridHeight = 22;
+    public static int gridWidth = 10;
+    
+    //Pertinent only to Tetrominoes
+    private Vector3 rotationPoint;
+    private float lastTime;
+    private float timeToFall = 0.9f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) 
+        {
+            transform.position += new Vector3(-1, 0, 0);
+            if (!isValidMove())
+            {
+                transform.position += new Vector3(1, 0, 0);
+            }
+        } else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+            if (!isValidMove())
+            {
+                Debug.Log(transform.position);
+                transform.position += new Vector3(-1, 0, 0);
+            }
+        } else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //This is the tetromino rotation
+            transform.RotateAround(transform.TransformPoint(rotationPoint),new Vector3(0,0,1),90);
+            if (!isValidMove())
+            {
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), -90);
+            }
+        }
+        
+        if(Time.time - lastTime > (Input.GetKey(KeyCode.DownArrow) ? timeToFall / 10 : timeToFall))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            if (!isValidMove())
+            {
+                transform.position += new Vector3(0, 1, 0);
+            }
+            lastTime = Time.time;
+        }
+    }
+
+    bool isValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int xRound = Mathf.RoundToInt(children.transform.position.x);
+            int yRound = Mathf.RoundToInt(children.transform.position.y);
+
+            if (xRound < 0 || xRound >= gridWidth || yRound < 0 || yRound >= gridHeight)
+            {
+                return false;
+            }
+            
+        }
+
+        return true;
+    }
+}
