f835c3cf99ec7a41743ea37e6eaa6ea36db01947
Ajax-dev <ajwilt97@gmail.com>
2021-03-09 16:38:18 +0000

    Functionality of Main Menu added, jumps into tetris game on play, can enter options screen, etc

---
+++ Assets/Scripts/Mechanics/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        Debug.Log("Playing game:" + (SceneManager.GetActiveScene().buildIndex + 1));
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quitting");
+        Application.Quit();
+    }
+}
