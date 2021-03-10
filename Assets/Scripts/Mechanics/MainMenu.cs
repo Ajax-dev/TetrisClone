945912c6d948e8d11b92c4bf52ae319039d58fc3
Ajax-dev <ajwilt97@gmail.com>
2021-03-10 16:15:56 +0000

    Menu
    Added functioning main menu, resolution changes seem not to work right now but can jump between fullscreen and non-fullscreen

--- Assets/Scripts/Mechanics/MainMenu.cs
+++ Assets/Scripts/Mechanics/MainMenu.cs
@@ -1,10 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
+
+    public AudioMixer masterMixer;
+    public TMP_Dropdown resolutionDropdown;
+    public TMP_Dropdown qualityDropdown;
+    private Resolution[] availableResolutions;
+
+    // Retrieve available resolutions
+    void Start()
+    {
+        //Set quality to default which is high
+        SetQuality(qualityDropdown.value);
+        availableResolutions = Screen.resolutions;
+        
+        resolutionDropdown.ClearOptions();
+
+        List<string> resolutionOptions = new List<string>();
+
+        int currentResIndex = 0;
+        for (int i = 0; i < availableResolutions.Length; i++)
+        {
+            string currentOption = availableResolutions[i].width + "x" + availableResolutions[i].height;
+            resolutionOptions.Add(currentOption);
+
+            if (availableResolutions[i].width == Screen.currentResolution.width && availableResolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+        // Takes in a list of strings so must reformat the array into nicely formatted strings
+        resolutionDropdown.AddOptions(resolutionOptions);
+        resolutionDropdown.value = currentResIndex;
+        resolutionDropdown.RefreshShownValue();
+
+
+    }
     public void PlayGame()
     {
         Debug.Log("Playing game:" + (SceneManager.GetActiveScene().buildIndex + 1));
@@ -15,5 +53,29 @@ public class MainMenu : MonoBehaviour
     {
         Debug.Log("Quitting");
         Application.Quit();
+    }
+    
+    // Options section
+    public void SetVolume(float volume)
+    {
+        masterMixer.SetFloat("masterVolume", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetQuality(int graphicIndex)
+    {
+        QualitySettings.SetQualityLevel(graphicIndex);
+        Debug.Log("Quality is now" + graphicIndex);
+    }
+
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetResolution(int resolutionIndex)
+    {
+        Resolution res = availableResolutions[resolutionIndex];
+        Screen.SetResolution(res.width, res.height, Screen.fullScreen);
     }
 }
